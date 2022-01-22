b9e6791705b6c4b5901b77998e3e0c5a31c0bf96
kchen01 <chenk4@carleton.edu>
2022-01-22 16:13:50 -0600

    .

--- kernel/proc.c
+++ kernel/proc.c
@@ -27,6 +27,32 @@ static err_t stack_setup(struct proc *p, char **argv, vaddr_t* ret_stackptr);
 /* tranlsates a kernel vaddr to a user stack address, assumes stack is a single page */
 #define USTACK_ADDR(addr) (pg_ofs(addr) + USTACK_UPPERBOUND - pg_size);
 
+// get a process by its PID.
+static struct proc* get_proc_by_pid(int pid){
+    spinlock_acquire(&ptable_lock);
+    for (Node *n = list_begin(&ptable); n != list_end(&ptable); n = list_next(n)) {
+        struct proc *p = list_entry(n, struct proc, proc_node);
+        if (p->pid == pid){
+            return p;
+        }
+    }
+    spinlock_release(&ptable_lock);
+    return NULL;
+}
+
+// finds an exited child of the process that is passed in
+static struct proc* find_exited_child(struct proc* parent){
+    spinlock_acquire(&ptable_lock);
+    for (Node *n = list_begin(&ptable); n != list_end(&ptable); n = list_next(n)) {
+        struct proc *p = list_entry(n, struct proc, proc_node);
+        if (p->parent->pid == parent->pid && p->exit_status == 1){
+            return p;
+        }
+    }
+    spinlock_release(&ptable_lock);
+    return NULL;
+}
+
 static struct proc*
 proc_alloc()
 {
@@ -105,16 +131,19 @@ proc_init(char* name)
         return NULL;
     }
 
+    // set default exit status
     p->exit_status = STATUS_ALIVE;
+    
+    // initialize condvar
+    condvar_init(p->wait_cv);
 
-    // initializes fileTable
+    // initialize fileTable
     p->fileTable[0] = &stdin;
     p->fileTable[1] = &stdout;
-    
     for(int i = 2; i < PROC_MAX_FILE; i++){
         p->fileTable[i] = NULL;
     }
-
+    
     return p;
 }
 
@@ -179,19 +208,22 @@ proc_fork()
     vaddr_t stackptr;
 
     if ((child = proc_init(parent->name)) == NULL) {
-        return ERR_NOMEM;
+        err = ERR_NOMEM;
     }
 
-    // load binary of the process
-    if ((err = proc_load(child, parent->name, &entry_point)) != ERR_OK) {
-        goto error;
-    }
+    // copy parent's memory to child
+    as_copy_as(&(parent->as), &child->as);
 
-    // set up stack and allocate its memregion 
-    if ((err = stack_setup(child, argv, &stackptr)) != ERR_OK) {
-        goto error;
+    // duplicate files from the parent process and reopen files that were open
+    int length = sizeof(parent->fileTable) / sizeof(parent->fileTable[0]);
+    for (int i = 0; i < length; i ++) {
+        if(parent->fileTable[i] != NULL){
+            child->fileTable[i] = parent->fileTable[i];
+            fs_reopen_file(child->fileTable[i]); 
+        }
     }
-
+  
+    // create new thread to run the process
     if ((t = thread_create(child->name, child, DEFAULT_PRI)) == NULL) {
         err = ERR_NOMEM;
         goto error;
@@ -202,22 +234,15 @@ proc_fork()
     list_append(&ptable, &child->proc_node);
     spinlock_release(&ptable_lock);
 
+    // set up trapframe for a new process
     tf_proc(t->tf, t->proc, entry_point, stackptr);
     *t->tf = *thread_current()->tf;
 
-    tf_set_return(t, 0);
+    // set return value in child to 0
+    tf_set_return(t->tf, 0);
 
-    as_copy_as(&(parent->as), &child->as);
-
-    for (int i = 0; i < length(parent->fileTable); i ++) {
-        if(parent->fileTable != NULL){
-            child->fileTable[i] = parent->fileTable[i];
-            //if(file open in parent){
-                fs_reopen_file(child->fileTable[i]);
-            //}
-        }
-    }
-
+    // set child's parent pointer
+    child->parent = parent;
     return child->pid;
 error:
     as_destroy(&child->as);
@@ -255,7 +280,38 @@ proc_detach_thread(struct thread *t)
 int
 proc_wait(pid_t pid, int* status)
 {
-    /* your code here */
+    if(!validate_ptr(status)){
+        return ERR_FAULT;
+    }
+
+    struct proc *p = proc_current();
+    struct proc *child;
+
+    // waiting for any child to exit
+    if (pid == -1){ 
+        spinlock_acquire(&ptable_lock);
+        while(!find_exited_child(p)){
+            condvar_wait(&child->wait_cv, &ptable_lock);
+        }
+        spinlock_release(&ptable_lock);
+
+    } 
+    // waiting on specific child
+    else { 
+        child = get_proc_by_pid(pid);
+        if (child == NULL){
+            return ERR_CHILD;
+        }
+        if (child->parent->pid != p->pid){
+            return ERR_CHILD;
+        }
+        spinlock_acquire(&ptable_lock);
+        while(child->exit_status == STATUS_ALIVE){
+            condvar_wait(&child->wait_cv, &ptable_lock);
+        }
+        spinlock_release(&ptable_lock);
+    }
+
     return pid;
 }
 
@@ -277,8 +333,32 @@ proc_exit(int status)
     fs_release_inode(p->cwd);
  
     /* your code here */
+    // close all open files for this process
+    for (int i = 0; i < length(p->fileTable); i ++) {
+        if(p->fileTable[i] != NULL){
+            fs_close_file(p->fileTable[i]);
+        }
+    }
 
+    // check process table to see which child processes have not finished and hand them off to init. 
+    for (Node *n = list_begin(&ptable); n != list_end(&ptable); n = list_next(n)) {
+        struct proc *cur_process = list_entry(n, struct proc, proc_node);
+        if (cur_process->parent->pid == p->pid && cur_process->exit_status == STATUS_ALIVE){
+            cur_process->parent = init_proc;
+        }
+    }
+
+    // set this process' exit status to 1
+    // figure out when exit status should be 0
+    p->exit_status = 1;
+
+    // change condition variable for the process
+    condvar_signal(p->wait_cv);
+
+    // cleanup other stuff
     thread_exit(status);
+    thread_cleanup(t);
+    proc_free(p);
 }
 
 /* helper function for loading process's binary into its address space */ 
